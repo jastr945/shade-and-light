4c66bccbcbc6677444738d7cebd773a0ce4513a5
jastr945 <polina.jastr@gmail.com>
2019-03-24 20:29:34 -0700

    Project boxes and parallax

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,47 +1,128 @@
 import React, { Component } from 'react';
 import './App.css';
+import { ParallaxProvider } from 'react-scroll-parallax';
+import { Parallax } from 'react-scroll-parallax';
 
 class App extends Component {
   render() {
     return (
+      <ParallaxProvider>
       <div className="App">
         <div className="mainContainer">
           <div className="header">
             <h3>Polina Jastrzebska</h3>
-            <p>Full-Stack Software Developer</p>
+            <p>{'{'}Full-Stack Software Developer{'}'}</p>
             <div className="social">
-              icon1
-              icon2
-              icon3
+              <img className="icon" src={require('./static/linkedin-icon.png')} width={50} alt="LinkedIn"/>
+              <img className="icon" src={require('./static/iconfinder_GITHUB_1217178.png')} width={50} alt="LinkedIn"/>
+              <img className="icon" src={require('./static/gmail.png')} width={50} alt="LinkedIn"/>
             </div>
           </div>
-          <div className="projects">
-            <div className="project">
-              <img className="projectPic"
-              src="https://pbs.twimg.com/profile_images/867761095909543937/hL_NQ7AZ_400x400.jpg" alt="project pic" />
-              <p>A short description of a project featuring some of the technologies and tools...</p>
-              eye-icon
-              git-icon-grey
+          <Parallax className="custom-class" x={[-25, 0]} tagOuter="figure">
+            <div className="projects">
+              <div className="project">
+                <div className="projectBox">
+                  <img className="projectPic"
+                  src="https://pbs.twimg.com/profile_images/867761095909543937/hL_NQ7AZ_400x400.jpg" alt="project pic" />
+                  <div className="projectInfo">
+                    <p>A short description of a project featuring some of the technologies and tools...</p>
+                    eye-icon
+                    git-icon-grey
+                  </div>
+                </div>
+              </div>
             </div>
-          </div>
-          <div className="projects">
-            <div className="project">
-              <img className="projectPic"
-              src="https://pbs.twimg.com/profile_images/867761095909543937/hL_NQ7AZ_400x400.jpg" alt="project pic" />
-              <p>A short description of a project featuring some of the technologies and tools...</p>
-              eye-icon
-              git-icon-grey
+          </Parallax>
+          <Parallax className="custom-class" x={[25, 0]} tagOuter="figure">
+            <div className="projects">
+              <div className="project">
+                <div className="projectBox">
+                  <div className="projectInfo">
+                    <p>A short description of a project featuring some of the technologies and tools...</p>
+                    eye-icon
+                    git-icon-grey
+                  </div>
+                  <img className="projectPic"
+                  src="https://pbs.twimg.com/profile_images/867761095909543937/hL_NQ7AZ_400x400.jpg" alt="project pic" />
+                </div>
+              </div>
             </div>
-          </div>
-          <div className="projects">
-            <div className="project">
-              <img className="projectPic"
-              src="https://pbs.twimg.com/profile_images/867761095909543937/hL_NQ7AZ_400x400.jpg" alt="project pic" />
-              <p>A short description of a project featuring some of the technologies and tools...</p>
-              eye-icon
-              git-icon-grey
+          </Parallax>
+          <Parallax className="custom-class" x={[-25, 0]} tagOuter="figure">
+            <div className="projects">
+              <div className="project">
+                <div className="projectBox">
+                  <img className="projectPic"
+                  src="https://pbs.twimg.com/profile_images/867761095909543937/hL_NQ7AZ_400x400.jpg" alt="project pic" />
+                  <div className="projectInfo">
+                    <p>A short description of a project featuring some of the technologies and tools...</p>
+                    eye-icon
+                    git-icon-grey
+                  </div>
+                </div>
+              </div>
             </div>
-          </div>
+          </Parallax>
+          <Parallax className="custom-class" x={[25, 0]} tagOuter="figure">
+            <div className="projects">
+              <div className="project">
+                <div className="projectBox">
+                  <div className="projectInfo">
+                    <p>A short description of a project featuring some of the technologies and tools...</p>
+                    eye-icon
+                    git-icon-grey
+                  </div>
+                  <img className="projectPic"
+                  src="https://pbs.twimg.com/profile_images/867761095909543937/hL_NQ7AZ_400x400.jpg" alt="project pic" />
+                </div>
+              </div>
+            </div>
+          </Parallax>
+          <Parallax className="custom-class" x={[-25, 0]} tagOuter="figure">
+            <div className="projects">
+              <div className="project">
+                <div className="projectBox">
+                  <img className="projectPic"
+                  src="https://pbs.twimg.com/profile_images/867761095909543937/hL_NQ7AZ_400x400.jpg" alt="project pic" />
+                  <div className="projectInfo">
+                    <p>A short description of a project featuring some of the technologies and tools...</p>
+                    eye-icon
+                    git-icon-grey
+                  </div>
+                </div>
+              </div>
+            </div>
+          </Parallax>
+          <Parallax className="custom-class" x={[10, 0]} tagOuter="figure">
+            <div className="projects">
+              <div className="project">
+                <div className="projectBox">
+                  <div className="projectInfo">
+                    <p>A short description of a project featuring some of the technologies and tools...</p>
+                    eye-icon
+                    git-icon-grey
+                  </div>
+                  <img className="projectPic"
+                  src="https://pbs.twimg.com/profile_images/867761095909543937/hL_NQ7AZ_400x400.jpg" alt="project pic" />
+                </div>
+              </div>
+            </div>
+          </Parallax>
+          <Parallax className="custom-class" x={[-10, 0]} tagOuter="figure">
+            <div className="projects">
+              <div className="project">
+                <div className="projectBox">
+                  <img className="projectPic"
+                  src="https://pbs.twimg.com/profile_images/867761095909543937/hL_NQ7AZ_400x400.jpg" alt="project pic" />
+                  <div className="projectInfo">
+                    <p>A short description of a project featuring some of the technologies and tools...</p>
+                    eye-icon
+                    git-icon-grey
+                  </div>
+                </div>
+              </div>
+            </div>
+          </Parallax>
           <footer>
             <small>&copy;
               {(new Date().getFullYear())}
@@ -49,6 +130,7 @@ class App extends Component {
           </footer>
         </div>
       </div>
+      </ParallaxProvider>
     );
   }
 }
