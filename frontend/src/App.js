adec81ff6c221db99c76666d7ef821ece871e2e8
jastr945 <polina.jastr@gmail.com>
2019-03-31 21:18:02 -0700

    Icons and zoom effects

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,9 @@ import { ParallaxProvider } from 'react-scroll-parallax';
 import { Parallax } from 'react-scroll-parallax';
 
 class App extends Component {
+  setButtonHovered(value) {
+    console.log(this);
+  }
   render() {
     return (
       <ParallaxProvider>
@@ -19,18 +22,21 @@ class App extends Component {
             </div>
           </div>
           <Parallax className="custom-class" x={[-25, 0]} tagOuter="figure">
-            <div className="projects">
+            <div className="projects"
+            onMouseEnter={this.setButtonHovered(true)}
+            onMouseLeave={this.setButtonHovered(false)}>
               <div className="project">
                 <div className="projectBox">
                   <img className="projectPic"
                   src="https://pbs.twimg.com/profile_images/867761095909543937/hL_NQ7AZ_400x400.jpg" alt="project pic" />
                   <div className="projectInfo">
                     <p>A short description of a project featuring some of the technologies and tools...</p>
-                    eye-icon
-                    git-icon-grey
+                    <img className="small-icon eye" src={require('./static/eye-2-512.gif')} alt="View Demo"/>
+                    <img className="small-icon" src={require('./static/70d584e3-4eb3-4ed1-8df8-944e9b123089.svg')} alt="View on GitHub"/>
                   </div>
                 </div>
               </div>
+              <div className="technologies">- - - - - - - - - - - - - - Python, Django, Docker, React</div>
             </div>
           </Parallax>
           <Parallax className="custom-class" x={[25, 0]} tagOuter="figure">
