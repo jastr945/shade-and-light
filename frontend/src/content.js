4d0647b38fd8618a3e4405c31af56a6d4745701f
jastr945 <polina.jastr@gmail.com>
2019-04-21 16:50:52 -0700

    Adding more projects

--- frontend/src/content.js
+++ frontend/src/content.js
@@ -12,15 +12,15 @@ var projects = [
     "repo": "http://codepen.io/jastr945/details/rJYGeB/"
   },
   {
-    "description": "test3: A short description of a project featuring some of the technologies and tools...",
+    "description": "This app uses crowd-sourced data to assist people in housing transit. I was a member of a team who created this project at the Hackathon organized by Wome Who Code and sponsored by Nike and Puppet. It took us 6h, and we became one of the winners!",
     "pic": "https://pbs.twimg.com/profile_images/867761095909543937/hL_NQ7AZ_400x400.jpg",
-    "technologies": "JavaScript, JQuery, web2py",
-    "repo": "http://codepen.io/jastr945/details/rJYGeB/",
-    "demo": "http://codepen.io/jastr945/details/rJYGeB/"
+    "technologies": "Node.js, Public API, Team Work",
+    "repo": "https://devpost.com/software/urban-amenities",
+    "demo": "https://shielded-shore-32899.herokuapp.com/"
   },
   {
     "description": "A responsive Django web app with a fast, full-text search engine powered by Elasticsearch library, along with elasticsearch-dsl  and django-elasticsearch-dsl Python clients.",
-    "pic": "https://pbs.twimg.com/profile_images/867761095909543937/hL_NQ7AZ_400x400.jpg",
+    "pic": "https://raw.githubusercontent.com/jastr945/shade-and-light/master/frontend/src/static/cannonbeach.jpg",
     "technologies": "Elasticsearch, Python, Django, Docker, Unit Testing",
     "repo": "https://github.com/jastr945/xperiments/tree/master/search/search_engine",
   },
