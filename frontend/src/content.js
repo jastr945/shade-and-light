ee9a2f45bfe4d114ba61a4464d60a7570b7f55cf
jastr945 <polina.jastr@gmail.com>
2019-04-21 17:07:59 -0700

    Adding more projects

--- frontend/src/content.js
+++ frontend/src/content.js
@@ -12,8 +12,8 @@ var projects = [
     "repo": "http://codepen.io/jastr945/details/rJYGeB/"
   },
   {
-    "description": "This app uses crowd-sourced data to assist people in housing transit. I was a member of a team who created this project at the Hackathon organized by Wome Who Code and sponsored by Nike and Puppet. It took us 6h, and we became one of the winners!",
-    "pic": "https://pbs.twimg.com/profile_images/867761095909543937/hL_NQ7AZ_400x400.jpg",
+    "description": "This app uses crowd-sourced data to assist people in housing transit. I was a member of a team who created this project at the Hackathon organized by Women Who Code and sponsored by Nike and Puppet. The work took us around 6 hours, and we became one of the winners!",
+    "pic": "https://raw.githubusercontent.com/jastr945/shade-and-light/master/frontend/src/static/urban_amenities.png",
     "technologies": "Node.js, Public API, Team Work",
     "repo": "https://devpost.com/software/urban-amenities",
     "demo": "https://shielded-shore-32899.herokuapp.com/"
@@ -28,6 +28,12 @@ var projects = [
     "description": "A simple login page layout created with React Router. This concise and straightforward piece of code has become of of my most viewed projects on CodePen.",
     "pic": "https://raw.githubusercontent.com/jastr945/shade-and-light/master/frontend/src/static/react-login.png",
     "technologies": "React, React-Router, Responsive Design",
+    "demo": "http://codepen.io/jastr945/details/rJYGeB/"
+  },
+  {
+    "description": "A Django website for an animal shelter, where the shelter staff can add and edit pet profiles. The app uses django-filters, ModelForms and SQL database queries for creating and searching records.",
+    "pic": "https://raw.githubusercontent.com/jastr945/shade-and-light/master/frontend/src/static/react-login.png",
+    "technologies": "Python, Django, Docker",
     "demo": "http://codepen.io/jastr945/details/rJYGeB/"
   }
 ];
